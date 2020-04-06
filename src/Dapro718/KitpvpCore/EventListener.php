ac2001bdbe92d5757a28ec229a3b5176a66c8988
Dapro718 <60823817+Dapro718@users.noreply.github.com>
2020-04-06 10:28:55 -0400

    Create EventListener.php

---
+++ src/Dapro718/KitpvpCore/EventListener.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dapro718\KitpvpCore
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\tile\Sign;
+
+class EventListener implements Listener {
+
+  public function onInteract(PlayerInteractEvent $event) {
+  		$block = $event->getBlock();
+		  $player = $event->getPlayer();
+		  $sign = $player->getLevel()->getTile($block);
+      $sign->setLine(0, "Test1");
+      $sign->setLine(1, "Test2");
+      $sign->setLine(2, "Test3");
+      $sign->setLine(3, "Test4");
+  }
+}
