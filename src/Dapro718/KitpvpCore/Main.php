26ce43bb6c1d9f15ea581ff2d57a033a39488300
Dapro718 <60823817+Dapro718@users.noreply.github.com>
2020-04-07 10:45:58 -0400

    Update Main.php

--- src/Dapro718/KitpvpCore/Main.php
+++ src/Dapro718/KitpvpCore/Main.php
@@ -16,27 +16,6 @@ class Main extends PluginBase implements Listener{
 
   public function onLoad(): void {
       $this->getLogger()->info("KitPvP Loaded");
-      $nOverworld_1 = 0;
-      $nOverworld_2 = 0;
-      $nOverworld_3 = 0;
-      $nDesert_1 = 0;
-      $nDesert_2 = 0;
-      $nDesert_3 = 0;
-      $nOcean_1 = 0;
-      $nOcean_2 = 0;
-      $nOcean_3 = 0;
-      $nJungle_1 = 0;
-      $nJungle_2 = 0;
-      $nJungle_3 = 0;
-      $nCity_1 = 0;
-      $nCity_2 = 0;
-      $nCity_3 = 0;
-      $nFuture_1 = 0;
-      $nFuture_2 = 0;
-      $nFuture_3 = 0;
-      $nAlien_1 = 0;
-      $nAlien_2 = 0;
-      $nAlien_3 = 0;
   }
   
   
