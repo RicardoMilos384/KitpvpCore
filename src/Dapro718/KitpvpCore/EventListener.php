8857ebcafc7f4d85526c93867ea827ffe56002d3
Dapro718 <60823817+Dapro718@users.noreply.github.com>
2020-04-07 09:39:34 -0400

    Update EventListener.php

--- src/Dapro718/KitpvpCore/EventListener.php
+++ src/Dapro718/KitpvpCore/EventListener.php
@@ -8,22 +8,20 @@ use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\tile\Sign;
 use pocketmine\block\SignChangeEvent;
-use _64FF00\PurePerms\data\UserDataManager
 
 class EventListener extends Sign implements Listener {
   
-  $prefix = "§l§8[§1KitPvP§8]§r"
+  $prefix = "§l§8[§1KitPvP§8]§r";
 
   public function onInteract(PlayerInteractEvent $event) {
       $block = $event->getBlock();
       $player = $event->getPlayer();
       $tile = $player->getLevel()->getTile($block);
-      $group = getGroup($player)
       if(!($tile instanceof Sign)) {
-        return;
-      } elseif $group === "Leather" or $group === "Chain" or $group === "Iron" {
-        
-        
+        $tile->setLine(0, "Hi Feather");
+        $tile->setLine(1, "Hi Feather");
+        $tile->setLine(2, "Hi Feather");
+        $tile->setLine(3, "Hi Feather");
       }
   }
 }
