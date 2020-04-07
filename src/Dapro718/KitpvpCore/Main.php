bf5d46170b2b4ebc1320468d2a3ff97423fcd0b5
Dapro718 <60823817+Dapro718@users.noreply.github.com>
2020-04-07 09:23:15 -0400

    Update Main.php

--- src/Dapro718/KitpvpCore/Main.php
+++ src/Dapro718/KitpvpCore/Main.php
@@ -8,6 +8,7 @@ use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 use pocketmine\plugin\Plugin;
 use pocketmine\Server;
+use Dapro718\KitpvpCore\utils\Data;
 
 class Main extends PluginBase implements Listener{
   
@@ -15,11 +16,32 @@ class Main extends PluginBase implements Listener{
 
   public function onLoad(): void {
       $this->getLogger()->info("KitPvP Loaded");
+      $nOverworld_1 = 0
+      $nOverworld_2 = 0
+      $nOverworld_3 = 0
+      $nDesert_1 = 0
+      $nDesert_2 = 0
+      $nDesert_3 = 0
+      $nOcean_1 = 0
+      $nOcean_2 = 0
+      $nOcean_3 = 0
+      $nJungle_1 = 0
+      $nJungle_2 = 0
+      $nJungle_3 = 0
+      $nCity_1 = 0
+      $nCity_2 = 0
+      $nCity_3 = 0
+      $nFuture_1 = 0
+      $nFuture_2 = 0
+      $nFuture_3 = 0
+      $nAlien_1 = 0
+      $nAlien_2 = 0
+      $nAlien_3 = 0
   }
   
   
   public function onEnable() {
-      $this->getLogger()->info("KitPvP Loaded");
+      $this->getLogger()->info("KitPvP Enabled");
   }
   
   
